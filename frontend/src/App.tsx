1827ccf099088a4673776202095b7bbd9b8f2b57
David Shen <me@pantherman594.com>
2020-08-24 01:32:50 -0400

    Fetch actual data if in production

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
-import { data } from './utils/dummy-data';
+import React, { useEffect, useState }from 'react';
+import superagent from 'superagent';
+
+import { data as dummyData } from './utils/dummy-data';
 import { CurrentPositiveChart, TestedAreaChart } from './components';
+import { CovidDataItem } from './types';
 import './App.css';
 
 export const App: React.FunctionComponent = () => {
+  const [data, setData] = useState<CovidDataItem[]>(dummyData);
+
+  useEffect(() => {
+    (async () => {
+      const res = await superagent.get('https://bccovid.dav.sh/data');
+
+      const newData = res.body.map((entry: any) => ({
+        ...entry,
+        date: new Date(entry.date),
+      }));
+
+      if (process.env.NODE_ENV === 'production') {
+        setData(newData);
+      }
+    })();
+  }, []);
+
   return (
     <div className="App">
       <h1>Boston College Covid-19 Live Statistics</h1>
